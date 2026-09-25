f26d5aeee7d1ba6c50740d073163b6fd3451c2f2
jagadiswari-307 <yandamurijagadeswari03@gmail.com>
2026-09-25 22:47:00 +0530

    v

--- game.js
+++ game.js
@@ -65,7 +65,7 @@ function showRoundIntro(roundNumberValue) {
 
     roundIntro.style.display = "flex";
     questionCard.style.display = "none";
-    nextBtn.style.display = "flex";
+    nextBtn.style.display = "none";
 
     roundNumber.textContent =
         `ROUND ${round.round} / ${person.questions.length}`;
